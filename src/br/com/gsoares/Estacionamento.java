7704163c353b19febe726541c2546a55573b78a1
gabrielluiz299 <gabrielluiz299@gmail.com>
2024-09-26 20:15:21 -0300

    atualização

--- src/br/com/gsoares/Estacionamento.java
+++ src/br/com/gsoares/Estacionamento.java
@@ -1,5 +1,36 @@
 package br.com.gsoares;
 
-public class Estacionamento {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Estacionamento<T extends Carro> {
+    private List<T> carrosEstacionados;
+
+    public Estacionamento() {
+        carrosEstacionados = new ArrayList<>();
+    }
+
+    public void estacionar(T carro) {
+        carrosEstacionados.add(carro);
+    }
+
+    public void listarCarros() {
+        for (T carro : carrosEstacionados) {
+            System.out.println(carro);
+        }
+    }
+
+    public static void main(String[] args) {
+        Estacionamento<Carro> estacionamento = new Estacionamento<>();
+
+        Carro gmCar = new GM("Chevrolet Cruze", "Preto");
+        Carro hondaCar = new Honda("Honda Fit", "Azul");
+        Carro vwCar = new Volkswagen("Volkswagen Golf", "Branco");
+
+        estacionamento.estacionar(gmCar);
+        estacionamento.estacionar(hondaCar);
+        estacionamento.estacionar(vwCar);
+
+        estacionamento.listarCarros();
+    }
 }
